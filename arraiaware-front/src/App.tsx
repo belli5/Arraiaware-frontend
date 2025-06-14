9ac680825729a96e9b103d407bde6fc20e3b74ca
GabrielViniciusss <biel23061@gmail.com>
2025-06-14 12:57:52 -0300

    chore: configure TailwindCSS with Vite

--- arraiaware-front/src/App.tsx
+++ arraiaware-front/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="min-h-screen bg-slate-900 flex flex-col justify-center items-center text-center text-white">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,19 +16,23 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1 className="text-5xl font-bold text-sky-400">Vite + React</h1>
+
+      <div className="card my-8"> 
+        <button
+          className="bg-sky-500 hover:bg-sky-600 text-red-500 font-bold py-2 px-4 rounded-lg"
+          onClick={() => setCount((count) => count + 1)}
+        >
+        count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className="mt-4"> 
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-slate-400">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
