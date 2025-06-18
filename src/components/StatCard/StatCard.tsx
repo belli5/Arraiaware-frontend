66d2667408456344131d115df24ae457c289678d
GabrielViniciusss <biel23061@gmail.com>
2025-06-18 20:38:49 -0300

    style: change some styles

--- src/components/StatCard/StatCard.tsx
+++ src/components/StatCard/StatCard.tsx
@@ -20,11 +20,11 @@ export default function StatCard({
   iconColor = 'text-gray-400'
 }: StatCardProps) {
   return (
-    <div className={`bg-white p-6 rounded-lg shadow-sm border-l-4 ${borderColor}`}>
+    <div className={`bg-white p-6 w-85 rounded-xl shadow-md border-l-4 hover:shadow-lg hover:-translate-y-1 transform transition ${borderColor}`}>
       <div className="flex justify-between items-start">
         <div className="flex flex-col text-left">
           <span className="text-xl font-semibold text-gray-800">{title}</span>
-          <span className={`text-2xl font-bold mt-1 ${valueColor}`}>{value}</span>
+          <span className={`text-2xl font-bold ${valueColor}`}>{value}</span>
           <span className="text-sm font-semibold text-gray-600 mt-1">{subtitle}</span>
         </div>
         <Icon className={`w-6 h-6 ${iconColor}`} />
