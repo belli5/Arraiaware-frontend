bd6d2fd3b8a8032f8afb88951cb0257101e59d91
GabrielViniciusss <biel23061@gmail.com>
2025-06-22 19:57:01 -0300

    chore: delete unused AvaliacaoLideres.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Avaliacao from './pages/Avaliacao'
-import AvaliacaoLideres from './pages/AvaliacaoLideres'
 import RH from './pages/RH';
 import Comite from './pages/Comite';
 
@@ -14,7 +13,6 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/avaliacao/:section" element={<Avaliacao />} />
-        <Route path="/avaliacaoLideres" element={<AvaliacaoLideres />} />
         <Route path="/RH" element= {<RH />} />
         <Route path="/Comite" element={<Comite />} />
       </Routes>
