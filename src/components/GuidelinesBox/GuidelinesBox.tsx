15844f6062c37536c469515ee8967eea287d7df6
GabrielViniciusss <biel23061@gmail.com>
2025-07-09 12:10:43 -0300

    feat: add batch criteria update

--- src/components/GuidelinesBox/GuidelinesBox.tsx
+++ src/components/GuidelinesBox/GuidelinesBox.tsx
@@ -3,10 +3,10 @@ export default function GuidelinesBox() {
     <div className="bg-amber-50 border-l-4 border-amber-400 p-4 rounded-r-lg">
       <h4 className="font-bold text-amber-800">Diretrizes para Importação:</h4>
       <ul className="list-disc list-inside mt-2 text-sm text-amber-700 space-y-1">
-          <li>Os arquivos devem conter colunas: Nome, Email, Departamento, Trilha, Nota, Data</li>
+          <li>Certifique-se de que os arquivos possuem dados do Usuário e de suas avaliações</li>
           <li>Certifique-se de que os dados estão validados antes do upload</li>
           <li>Históricos duplicados serão automaticamente identificados</li>
-          <li>O processamento pode levar alguns minutos para arquivos grandes</li>
+          <li>O processamento pode levar alguns minutos muitos arquivos</li>
       </ul>
     </div>
   );
